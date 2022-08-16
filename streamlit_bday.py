87f48f65906c30a864de11bbbb64739ef2d631e5
a-pearlman <96663441+a-pearlman@users.noreply.github.com>
2022-08-16 22:11:31 +0100

    Create streamlit_bday.py

---
+++ streamlit_bday.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.title("When's the next Birthday??")
+
+df = pd.read_csv('C:/Users/Abigail.Pearlman/OneDrive - Gatwick Airport Limited/Birthdays.csv')
+
+date = dt.date.today()
+
+def g(x):
+    y = x.split('-')
+    y[0] = int(y[0])
+    y[1] = int(y[1])
+    y[2] = int(y[2])
+    z = dt.date(date.year, y[1], y[2])
+    d = z - date
+    if  z < date:
+        p = d + dt.timedelta(365)
+    else:
+        p = d
+    return p
+ 
+df['days'] = df['Birthday'].apply(g)
+
+name = df[df['days'] == df['days'].min()]['Name']
+date1 = df[df['days'] == df['days'].min()]['Birthday']
+days = df[df['days'] == df['days'].min()]['days']
+
+n = list(df['Name'])
+
+st.text("The next Birthday is...")
+button = st.button("Click me!")
+if button:
+    st.balloons()
+    st.text(f'{name}\'s birthday is in {days} days on {date1}')
+
+st.text("Looking for someone's Birthday?")
+e = st.selectbox('Name', n)
+v = df['Birthday'][e]
+st.text(f'{e}\'s birthday is on {v}')
+
+st.text('Show me everyone\'s Birthday?')
+b = st.button('Yes!')
+if b:
+    df
